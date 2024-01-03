6b29be8b321f5a5559e0b67dd78f4e3c971327f0
Søren Peter Loff Christensen <146208455+Loff-code@users.noreply.github.com>
2024-01-03 16:15:48 +0100

    new

--- Script/index_script.js
+++ Script/index_script.js
@@ -1,8 +1,8 @@
-function displayText(){
+function displayText() {
     var inputText = document.getElementById('textInput').value;
     var displayArea = document.getElementById('displayArea');
-    displayArea.innerHTML = "<p>" + inputText + "</p>";
+    displayArea.innerHTML = "<p>" + inputText + ", you are gay!" + "</p>";
 
-   
+
 }
 
