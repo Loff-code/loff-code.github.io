4f5b0e7bb436928673acd4c69500af3c76ec96f6
Søren Peter Loff Christensen <146208455+Loff-code@users.noreply.github.com>
2024-03-11 14:21:54 +0100

    Update heading.js

--- Script/heading.js
+++ Script/heading.js
@@ -2,20 +2,20 @@ const headerTemplate = document.createElement("template");
 
 headerTemplate.innerHTML = `
   <style>
-    @import url("/loffy.dk/Style/header.css");
+    @import url("/Style/header.css");
   </style>
 
   <header>
     <div class="blog-header">
-      <a href="/loffy.dk/index.html" class="logo-link">
-        <img class="logo-img" src="/loffy.dk/images/Logo.png" alt="Logo" />
+      <a href="/index.html" class="logo-link">
+        <img class="logo-img" src="/images/Logo.png" alt="Logo" />
       </a>
 
       <div class="dropdown">
         <button class="dropbtn"></button>
         <div class="dropdown-content">
           <a class="download" href="/loffy.dk/Pages/Downloads.html">Flight Scrapers</a>
-          <a class="next" href="/loffy.dk/Pages/FuckU.html">Fuck You Dudee</a>
+          <a class="next" href="/Pages/FuckU.html">Fuck You Dudee</a>
           <a class="converter" href="/loffy.dk/Pages/Link2Down.html">Convert Download Link</a>
           <a class="converter" href="/loffy.dk/Pages/UnitConverter.html">Convert Units</a>
           <a class="next" href="/loffy.dk/Pages/Form_upload.php">Upload Form</a>
